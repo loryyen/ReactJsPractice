9af9c829a6025b36587975ec048ff963c5619f29
lory <hueling.yen@gmail.com>
2019-01-25 19:20:00 +0800

    add

---
+++ counter/src/reducer.js
@@ -0,0 +1,10 @@
+const intialState = {
+    count: 1
+};
+
+export default function(state = intialState, action) {
+    switch (action.name) {
+        default: 
+            return state;
+    }
+}
