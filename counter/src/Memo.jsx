0a1be6a914f8d7eb64aaf85fd1d1aaf915da0b55
lory <hueling.yen@gmail.com>
2019-01-25 19:04:03 +0800

    add Redux

---
+++ counter/src/Memo.jsx
@@ -0,0 +1,15 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+
+class Memo extends Component {
+   
+    render() {
+        return (
+            <div>
+               
+            </div>
+        );
+    }
+}
+
+export default connect(state => state)(Memo);
